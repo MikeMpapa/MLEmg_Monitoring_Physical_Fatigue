3bbd562c32ad6592c1469ce89e20a0749e32d418
MikeMpapa <michalis.papakostas@mavs.uta.edu>
2018-09-25 20:54:46 -0500

    extenssive cross validation

--- src/EMG_training.py
+++ src/EMG_training.py
@@ -27,33 +27,37 @@ def showEMGData(data,duration,gt):
     plt.show()
 
 
-def featureExtraction(raw_data,time):
+def featureExtraction(raw_data,time,gt_labels):
     #emg_features_vectors = []
-    for idx,sample in enumerate(raw_data):
-        duration = float(time[idx][-1]-time[idx][0])
-        Fs = round(len(sample) / duration)
-        mtWin = 5.0
-        mtStep = 1.0
-        stWin = 0.5
-        stStep = 0.5
-        #print 'FS =',Fs
-        [MidTermFeatures, stFeatures] = f1d.mtFeatureExtraction(sample, Fs, round(mtWin * Fs), round(mtStep * Fs), round(Fs * stWin), round(Fs * stStep))
+    duration = float(time[-1]-time[0])
+    Fs = round(len(raw_data) / duration)
+    mtWin = 5.0
+    mtStep = 1.0
+    stWin = 0.5
+    stStep = 0.5
 
-        if idx == 0:
-            emg_features_vectors = MidTermFeatures.copy()
-        else:
-            emg_features_vectors =  np.concatenate((emg_features_vectors, MidTermFeatures),axis=1)
+    [MidTermFeatures, stFeatures] = f1d.mtFeatureExtraction(raw_data, Fs, round(mtWin * Fs), round(mtStep * Fs), round(Fs * stWin), round(Fs * stStep))
 
-        #TODO: NOW EACH CSV IS REPRESENTED BY A FV --> NOT THAT
+    emg_features_vectors = MidTermFeatures.copy()
 
-        numOfFeatures = len(stFeatures)
-        numOfMidTerm = len(MidTermFeatures)
+    # Assign labels to mid-term windows
+    numOfmtWindows = int(round(len(gt_labels) / Fs))
+    gt_WindowLabels = []
+    cur = 0
+    N = int(Fs)
+    for w in range(numOfmtWindows):
+            if N > len(gt_labels):
+                N = len(gt_labels) - 1
+            c0 = gt_labels[cur:N].count(0)  # count no Fatigue labels
+            c1 = gt_labels[cur:N].count(1)  # count Fatigue labels
+            if c0 > c1:
+                gt_WindowLabels.append(0)
+            else:
+                gt_WindowLabels.append(1)
+            cur = N
+            N = N + int(Fs)
 
-        #Fx = MidTermFeatures.mean(axis=1)
-        #emg_features_vectors.append(Fx)
-
-        #emg_features_vectors.append(Fx)
-    return emg_features_vectors
+    return emg_features_vectors,gt_WindowLabels
 
 
 
@@ -61,6 +65,8 @@ def featureExtraction(raw_data,time):
 
 
 def evaluateClassifier(argv):
+    save = argv[5]
+
     dirName = argv[2]    # path to csv files
     fileList  = sorted(glob.glob(os.path.join(dirName, "*.csv")))
 
@@ -69,28 +75,15 @@ def evaluateClassifier(argv):
     user = []
     exercise = []
     repetition = []
-    time_fatigue = []
-    time_nofatigue = []
     time = []
-    emg_raw_fatigue = []
-    emg_raw_nofatigue = []
     emg_raw = []
     gt_labels = []
-    for i, m in enumerate(fileList):
+    feature_vectors_nofatigue = []
+    feature_vectors_fatigue = []
 
+    for file in fileList:
 
-        '''
-        if user not in data['user'].keys():
-            data['user'][user]={}
-            data['user'][user]['exercise'] = {}
-        if exercise not in data['user'][user]['exercise'].keys():
-            data['user'][user]['exercise'][exercise] = {}
-            data['user'][user]['exercise'][exercise]['repetition'] = {}
-        if repetition not in data['user'][user]['exercise'][exercise]['repetition'].keys():
-            data['user'][user]['exercise'][exercise]['repetition'][repetition] = []
-        '''
-
-        with open(m,'r') as f:
+        with open(file,'r') as f:
             x = f.readlines()
             if not x:
                 continue
@@ -100,71 +93,75 @@ def evaluateClassifier(argv):
         f.close
 
         #split the sample into the positive and negative classes
+        ###
+        feature_vectors,gtWindowLabels = featureExtraction(emg_raw[-1], time[-1],gt_labels[-1])
 
-        split = gt_labels[-1].index(1)
-        emg_raw_fatigue.append(emg_raw[-1][:split])
-        emg_raw_nofatigue.append(emg_raw[-1][split:])
-        time_fatigue.append(time[-1][:split])
-        time_nofatigue.append(time[-1][split:])
+        for i,w in enumerate(gtWindowLabels):
+            if w==0:
+                feature_vectors_nofatigue.append( feature_vectors[:,i])
+            else:
+                feature_vectors_fatigue.append(feature_vectors[:,i])
 
-        user.append(m.split('/')[-1].split('E')[0][1:])
-        exercise.append(m.split('/')[-1].split('R')[0][-1])
-        repetition.append(m.split('/')[-1].split('.')[0][-1])
+
+        user.append(file.split('/')[-1].split('E')[0][1:])
+        exercise.append(file.split('/')[-1].split('R')[0][-1])
+        repetition.append(file.split('/')[-1].split('.')[0][-1])
 
 
         if argv[-1] == '-s':
             showEMGData(emg_raw[-1],time[-1][-1]-time[-1][0],gt_labels[-1])
 
-        #data['user'][user]['exercise'][exercise]['repetition'][repetition] = [time,emg,gt]
-
-    users_for_training = math.ceil(len(set(user))*0.8)
-    #print users_for_training
-
-    #Extract no_fatigue features
-    feature_vectors_nofatigue = np.transpose(featureExtraction(emg_raw_nofatigue,time_nofatigue))
-    no_fatigue_labels = [0]*feature_vectors_nofatigue.shape[1]
-
-    #Extract fatigue features
-    feature_vectors_fatigue = np.transpose(featureExtraction(emg_raw_fatigue,time_fatigue))
-    fatigue_labels = [1]*feature_vectors_fatigue.shape[1]
-
 
     #Collect all features
-    #featuresAll = list(np.transpose(np.concatenate((feature_vectors_nofatigue,feature_vectors_fatigue),axis=1)))
     featuresAll = []
-    featuresAll.append(feature_vectors_nofatigue)
-    featuresAll.append(feature_vectors_fatigue)
-    labelsAll = ['0','1'] # 0:NofFtigue, 1:Fatigue
+    featuresAll.append(np.array(feature_vectors_nofatigue))
+    featuresAll.append(np.array(feature_vectors_fatigue))
+    labelsAll = ['0:NoFtigue','1:Fatigue'] # 0:NoFtigue, 1:Fatigue
 
     #Normilize features
     (featuresAll, MEAN, STD) = aT.normalizeFeatures(featuresAll)
 
     clf = argv[3][1:]
     params = argv[4]
-    #bestParam = aT.evaluateclassifier(featuresAll, labelsAll, 1000, clf , [0.05, 0.1, 0.5], 0, perTrain=0.80)
     bestParam = aT.evaluateclassifier(featuresAll, labelsAll, 1000, clf , params, 0, perTrain=0.80)
 
 
     MEAN = MEAN.tolist()
     STD = STD.tolist()
 
+    if clf == 'svm':
+        model = aT.trainSVM(featuresAll, bestParam)
+    elif   clf == 'svm_rbf':
+        model = aT.trainSVM_RBF(featuresAll, bestParam)
+    elif clf == 'extratrees':
+        model = aT.trainExtraTrees(featuresAll, bestParam)
+    elif clf == 'randomforest':
+        model = aT.trainRandomForest(featuresAll, bestParam)
+    elif clf == 'knn':
+        model = aT.trainKNN(featuresAll, bestParam)
+    elif clf == 'gradientboosting':
+        model = aT.trainGradientBoosting(featuresAll, bestParam)
+
+
+    if save:
+        saveClassifier(clf,bestParam,model,MEAN,STD,labelsAll)
+
+    print 'Training of',clf,'completed'
+
+    return clf,model,labelsAll,MEAN,STD,bestParam
+
+
+
+def saveClassifier(clf_name,bestParam,model,MEAN,STD,labelsAll):
     # STEP C: Save the classifier to file
-    Classifier = aT.trainSVM(featuresAll, bestParam)
-    modelName = clf +'_'+str(bestParam)
+    modelName = clf_name + '_' + str(bestParam)
     with open(modelName, 'wb') as fid:  # save to file
-        cPickle.dump(Classifier, fid)
+        cPickle.dump(model, fid)
     fo = open(modelName + "MEANS", "wb")
     cPickle.dump(MEAN, fo, protocol=cPickle.HIGHEST_PROTOCOL)
     cPickle.dump(STD, fo, protocol=cPickle.HIGHEST_PROTOCOL)
     cPickle.dump(labelsAll, fo, protocol=cPickle.HIGHEST_PROTOCOL)
     fo.close()
-
-
-
-    print 'Training of',clf,'completed'
-
-
-
 
 
 
--- src/FatigueMonitoring.py
+++ src/FatigueMonitoring.py
@@ -1,4 +1,128 @@
 from EMG_training import evaluateClassifier
+from EMG_training import featureExtraction
+import os
+import glob
+import numpy
+from pyAudioAnalysis import audioTrainTest as aT
+from scipy.signal import medfilt
+
+
+
+
+def classifySingleFile(fileName,clf_name,model,MEAN,STD,classNames,filter):
+    emg_raw = []
+    time=[]
+    gt_labels = []
+    CM_file = numpy.zeros((len(classNames), len(classNames)))
+
+    with open(fileName, 'r') as f:
+        x = f.readlines()
+        if  not x:
+            return CM_file
+        time.append([float(label.split(',')[0]) for label in x])
+        emg_raw.append([float(label.split(',')[1]) for label in x])
+        gt_labels.append([int(label.split(',')[2].rstrip()) for label in x])
+    f.close
+
+    ############
+    '''
+    mtWin = 5.0
+    mtStep = 1.0
+    stWin = 0.5
+    stStep = 0.5
+    mtWinFs = round(mtWin * Fs)
+    mtStepFs = round(mtStep * Fs)
+    stWinFs = round(Fs * stWin)
+    stStepFs = round(Fs * stStep)
+    mtWinRatio = int(round(mtWinFs / stStepFs))
+    mtStepRatio = int(round(mtStepFs / stStepFs))
+    '''
+    ##################
+
+
+    fVs,labels = featureExtraction(emg_raw[0],time[0],gt_labels[0])
+    MEAN = numpy.array(MEAN)
+    STD = numpy.array(STD)
+
+
+    predictions = []
+    for i in range(fVs.shape[1]):
+        fV = fVs[:,i]
+        fV = (fV - MEAN) / STD
+        [Result, P] = aT.classifierWrapper(model, clf_name, fV)  # classification
+        predictions.append(Result)
+
+    if filter:
+        predictions = medfilt(predictions,13)
+
+    for idx,p in enumerate(predictions):
+        CM_file[int(labels[idx]),int(p)] += 1
+
+    print 'Classification Results for file:',fileName
+    print CM_file
+    print
+
+    return CM_file
+
+
+
+
+
+
+
+def classifyDir(dirName,clf_name,trained_classifier,classNames,MEAN,STD,filter):
+    fileList = sorted(glob.glob(os.path.join(dirName, "*.csv")))
+
+    CM = numpy.zeros((len(classNames), len(classNames)))
+
+    for idx, file in enumerate(fileList):
+        result = classifySingleFile(file,clf_name,trained_classifier,MEAN,STD,classNames,filter)
+        CM += result
+
+    return CM
+
+
+
+def computeEvalMetrics(CM,clf_name,bestparam):
+
+    CM = CM + 0.0000000010
+
+    Rec = numpy.zeros((CM.shape[0],))
+    Pre = numpy.zeros((CM.shape[0],))
+
+    for ci in range(CM.shape[0]):
+        Rec[ci] = CM[ci, ci] / numpy.sum(CM[ci, :])
+        Pre[ci] = CM[ci, ci] / numpy.sum(CM[:, ci])
+    F1 = 2 * Rec * Pre / (Rec + Pre)
+    print 'Total CM'
+    print CM
+    print 'Pre:',Pre,'- AVG Pre:',numpy.mean(Pre)
+    print 'Rec:',Rec,'- AVG Rec:',numpy.mean(Rec)
+    print 'F1:',F1,'- AVG F1:',numpy.mean(F1)
+    numpy.save(clf_name +"_"+str(bestparam) +"_results.npy", CM)
+
+
+
+
+def EMG_Train_Test(evaluation_path,classifier,params,filter):
+
+    folds = sorted(glob.glob(os.path.join(evaluation_path, "fold*")))
+    CM_total = None
+    for idx,fold in enumerate(folds):
+        #Train
+        #clf_name,trained_classifier,labelsAll,MEAN,STD = evaluateClassifier([None, '-c', fold+'/train', '-extratrees', [10, 25, 50, 100, 200, 500, 1000], False])
+        clf_name,trained_classifier,labelsAll,MEAN,STD,bestParam = evaluateClassifier([None, '-c', fold+'/train', classifier, params, False])
+        #Test
+        CM = classifyDir(fold+'/test', clf_name, trained_classifier, labelsAll, MEAN, STD,filter)
+        if idx==0:
+            CM_total = CM
+        else:
+            CM_total += CM
+    computeEvalMetrics(CM_total,clf_name,bestParam)
+
+
+
+
 
 
 if __name__ == '__main__':
@@ -8,7 +132,10 @@ if __name__ == '__main__':
     #evaluateClassifier([None,'-c','../Fatigue_Data/Study1/EMG', '-knn',[1, 3, 5, 7, 9, 11, 13, 15]])
     #evaluateClassifier([None,'-c','../Fatigue_Data/Study1/EMG', '-randomforest',[10, 25, 50, 100,200,500,1000]])
     #evaluateClassifier([None,'-c','../Fatigue_Data/Study1/EMG', '-gradientboosting',[10, 25, 50, 100,200,500,1000]])
-    evaluateClassifier([None,'-c','../Fatigue_Data/Study1/EMG', '-extratrees',[10, 25, 50, 100,200,500,1000]])
-
+    #evaluateClassifier([None,'-c','../Fatigue_Data/Study1/EMG', '-extratrees',[10, 25, 50, 100,200,500,1000],False])
+    clfs = ['-svm','-svm_rbf','-knn','-randomforest','-gradientboosting','-extratrees']
+    params = [[0.001, 0.01,  0.5, 1.0, 5.0, 10.0, 20.0],[0.001, 0.01,  0.5, 1.0, 5.0, 10.0, 20.0],[1, 3, 5, 7, 9, 11, 13, 15],[10, 25, 50, 100,200,500,1000],[10, 25, 50, 100,200,500,1000],[10, 25, 50, 100,200,500,1000]]
+    for i in range(len(clfs)):
+        EMG_Train_Test('EMG_Data_Study1',clfs[i],params[i],False)
 
 
